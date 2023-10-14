ee9bf6a898fa350eea96b3cd4fef092d329211e8
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-10-13 21:13:19 -0600

    Update and rename PokemonFuego to PokemonFuego.java

---
+++ Pokemon_Abstraccion/Pokemon/PokemonFuego.java
@@ -0,0 +1,40 @@
+package pokemon.pokemon;
+
+public class PokemonFuego extends Pokemon {
+    public PokemonFuego(String nombre, int nivel) {
+        super(nombre, Tipo.FUEGO, nivel);
+
+        ListaMovimientos listaMovimientos = new ListaMovimientos();
+
+        Movimiento[] movimientos = new Movimiento[4];
+        movimientos[0] = listaMovimientos.buscarMovimientoPorNombre("Ascuas");
+        movimientos[1] = listaMovimientos.buscarMovimientoPorNombre("Colmillo igneo");
+        movimientos[2] = listaMovimientos.buscarMovimientoPorNombre("Giro Fuego");
+        movimientos[3] = listaMovimientos.buscarMovimientoPorNombre("Evite igneo");
+
+        setMovimientos(movimientos);
+    }
+
+    @Override
+    public double obtenerEfectividad(Pokemon pokemon) {
+        double efectividad = 1.0;
+
+        //No efectivo
+        if (pokemon.getTipo() == Tipo.AGUA || 
+            pokemon.getTipo() == Tipo.ROCA || 
+            pokemon.getTipo() == Tipo.FUEGO || 
+            pokemon.getTipo() == Tipo.DRAGON) {
+            efectividad = 0.0;
+        }
+
+        //Efectivo
+        if (pokemon.getTipo() == Tipo.BICHO || 
+            pokemon.getTipo() == Tipo.PLANTA || 
+            pokemon.getTipo() == Tipo.ACERO || 
+            pokemon.getTipo() == Tipo.HIELO) {
+            efectividad = 2.0;
+        }
+
+        return efectividad;        
+    }
+}
