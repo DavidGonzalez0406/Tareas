3badc3365c4d79bb34d9d698ce0f6eb2bc31e41b
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-10-13 21:11:52 -0600

    Update Movimiento.java

--- Pokemon_Abstraccion/Pokemon/Movimiento.java
+++ Pokemon_Abstraccion/Pokemon/Movimiento.java
@@ -1,76 +1,46 @@
 package pokemon.pokemon;
 
-public abstract class Pokemon {
-    private int HP;
-    private int nivel;
+public class Movimiento {
     private String nombre;
+    private int puntosDeAtaque;
+    private int pp;
     private Tipo tipo;
-    private Movimiento movimientos[];
 
-    public Pokemon(String nombre, Tipo tipo, int nivel) {
-        this.HP = 250;
+public Movimiento(String nombre, int puntosDeAtaque, Tipo tipo, int pp) {
         this.nombre = nombre;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.pp = pp;
         this.tipo = tipo;
-        this.nivel = nivel;
-        this.movimientos = new Movimiento[4];
-    }
-
-    public int getHP() {
-        return HP;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public int getPuntosDeAtaque() {
+        return puntosDeAtaque;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
     public Tipo getTipo() {
         return tipo;
     }
 
-    public int getNivel() {
-        return nivel;
+    public void setPp(int pp) {
+        this.pp = pp;
     }
 
-    public Movimiento[] getMovimientos() {
-        return movimientos;
-    }
+private Movimiento movimientos[];
 
-    public void setMovimientos(Movimiento[] movimientos) {
-        this.movimientos = movimientos;
-    }
+public void setMovimiento(int indice, Movimiento movimiento) {
+    movimientos[indice] = movimiento;
+}
 
-    
-    private void calculaDanio(int danio, double efectividad) {
-        double puntosRestados = danio * efectividad;
-        this.HP -= puntosRestados;
-        System.out.printf("%s recibe %.2f puntos de daño\n", this.getNombre(), puntosRestados);
-    }
+public Movimiento getMovimiento(int indice) {
+    return movimientos[indice];
+}
 
-    public void recibirAtaques(Movimiento movimiento, double efectividad) {
-        System.out.printf("%s recibe ATK %s\n", this.getNombre(), movimiento.getNombre());
-        calculaDanio(movimiento.getPuntosDeAtaque(), efectividad);
-    }
-
-    protected boolean seHaConcretadoAtaque(Movimiento movimiento, Pokemon pokemon) {
-        System.out.printf("%s ataca %s con %s\n", this.getNombre(), pokemon.getNombre(), movimiento.getNombre());
-        double efectividad = obtenerEfectividad(pokemon);
-
-        if (movimiento.getPp() > 0) {
-            pokemon.recibirAtaques(movimiento, efectividad);
-            return true;
-        } else {
-            System.err.println("El movimiento no tiene puntos de PP");
-            return false;
-        }
-    }
-
-    public void atacar(int m, Pokemon pokemon) {
-        Movimiento movimiento = getMovimientos()[m]; // Utiliza corchetes para acceder a un elemento específico del arreglo
-        boolean seHaConcretadoAtaque = seHaConcretadoAtaque(movimiento, pokemon);
-        if (seHaConcretadoAtaque) {
-            movimiento.setPp(movimiento.getPp() - 1);
-        }
-    }
-
-    public abstract double obtenerEfectividad(Pokemon pokemon);
 }
