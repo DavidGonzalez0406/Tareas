0d0be4b64634ccaf2121b40b956535aef4640657
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-10-27 22:22:46 -0600

    Create Movimiento.java

---
+++ Pokemon_Polimorfismo/movimientos/Movimiento.java
@@ -0,0 +1,52 @@
+package pokemon.pokemon;
+
+public class Movimiento {
+    private String nombre;
+    private int puntosDeAtaque;
+    private int pp;
+    private Tipo tipo;
+    private TipoAtaque TipoAtaque;
+
+    Movimiento(String nombre, int puntosDeAtaque, Tipo tipo, int pp, TipoAtaque TipoAtaque) {
+        this.nombre = nombre;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.pp = pp;
+        this.tipo = tipo;
+        this.TipoAtaque = TipoAtaque;
+        this.movimientos = movimientos;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntosDeAtaque() {
+        return puntosDeAtaque;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public TipoAtaque getTipoAtaque() {
+        return TipoAtaque;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+
+private Movimiento movimientos[];
+
+public void setMovimiento(int indice, Movimiento movimiento) {
+    movimientos[indice] = movimiento;
+}
+
+public Movimiento getMovimiento(int indice) {
+    return movimientos[indice];
+}
+
+}
