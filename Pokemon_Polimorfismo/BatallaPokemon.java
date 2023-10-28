9ead956e699b6e08208d03fc8591f4397a999736
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-10-27 22:17:59 -0600

    Create BatallaPokemon.java

---
+++ Pokemon_Polimorfismo/BatallaPokemon.java
@@ -0,0 +1,16 @@
+package pokemon;
+
+import pokemon.pokemon.*;
+
+public class BatallaPokemon {
+public static void main(String[] args) {
+      Mew mewInstancia = new Mew(Tipo.PSIQUICO, 100, 100, 100, 100, 100, 100, 10);
+      Articuno articunoInstancia = new Articuno(Tipo.HIELO.VOLADOR, 90, 85, 100, 95, 125, 85, 10);
+   
+  mewInstancia.atacar(2, articunoInstancia);
+  articunoInstancia.atacar(1, mewInstancia);
+
+  System.out.println(Mew.class.getSimpleName() + ": PS " + mewInstancia.getPS());
+  System.out.println(Articuno.class.getSimpleName() + ": PS " + articunoInstancia.getPS());
+    }
+}
